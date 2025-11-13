ebaa5c7e0f8381e8d327255803707143e292402c
Amine El Haouat <elmadridi69@gmail.com>
2025-11-13 12:07:39 +0100

    Getting Started

---
+++ clustered-data-warehouse/src/test/java/com/amine/fx/ClusteredDataWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amine.fx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusteredDataWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
